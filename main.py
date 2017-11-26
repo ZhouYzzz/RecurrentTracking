0fbf5c469003555338eb80d73cde538f79e27c18
Yizhuang Zhou <zhouyz9608@gmail.com>
2017-11-26 23:24:16 +0800

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+
+flags = tf.flags
+logging = tf.logging
+
+flags.DEFINE_string("data_path", '/home/spark/data/ILSVRC2015/ILSVRC2015',
+                    "Where the training/test/val data is stored.")
+FLAGS = flags.FLAGS
+
+def main(_):
+  print tf.gfile.Exists(FLAGS.data_path)
+
+if __name__ == '__main__':
+  tf.app.run()
