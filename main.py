5331d528ec39df939f08c06b02981aae5c1b80ec
Yizhuang Zhou <zhouyz9608@gmail.com>
2017-11-27 12:01:34 +0800

    main

--- main.py
+++ main.py
@@ -17,12 +17,9 @@ def read_anno_from_file(filename):
   size = tree.getroot().find('size')
   width = np.float32(size.find('width').text)
   height = np.float32(size.find('height').text)
-  bndbox = tree.getroot().find('object').find('bndbox')
-  xmax = np.float32(bndbox.find('xmax').text)
-  xmin = np.float32(bndbox.find('xmin').text)
-  ymax = np.float32(bndbox.find('ymax').text)
-  ymin = np.float32(bndbox.find('ymin').text)
-  return np.array([width,height,xmax,xmin,ymax,ymin])
+  objects = tree.getroot().findall('object')
+  annos = np.array([[float(x.text) for x in obj.find('bndbox').getchildren()] for obj in objects],np.float32)
+  return [np.array([width,height]), annos]#,xmax,xmin,ymax,ymin])
 
 def main(_):
   print tf.gfile.Exists(FLAGS.data_path)
@@ -35,12 +32,42 @@ def main(_):
   ImageSets_train_filename_queue = tf.train.string_input_producer(ImageSets_train_filename)
   reader = tf.TextLineReader()
   key, value = reader.read(ImageSets_train_filename_queue)
+  
   record_defaults = [['/path/prefix/to/record'],[1]]
   ImageSets_train_prefix, _ = tf.decode_csv(
     value, record_defaults=record_defaults, field_delim=' ')
+
+
   Annotations_train_filename = tf.string_join(
     [FLAGS.data_path,'Annotations','VID','train',ImageSets_train_prefix,'000000.xml'], separator='/')
-  Annotations_train = tf.py_func(read_anno_from_file, [Annotations_train_filename], tf.float32)
+  Images_train_filename = tf.string_join(
+    [FLAGS.data_path,'Data','VID','train',ImageSets_train_prefix,'000000.JPEG'], separator='/')
+  
+  num_threads = 10
+  batch_size = 100
+  Annotations_train_filename_queue = tf.FIFOQueue(10000, dtypes=tf.string)
+  Annotations_enqueue = Annotations_train_filename_queue.enqueue(Annotations_train_filename)
+  tf.train.add_queue_runner(
+          tf.train.QueueRunner(Annotations_train_filename_queue, [Annotations_enqueue] * num_threads))
+  Images_train_filename_queue = tf.FIFOQueue(10000, dtypes=tf.string)
+  Images_enqueue = Images_train_filename_queue.enqueue(Images_train_filename)
+  tf.train.add_queue_runner(
+          tf.train.QueueRunner(Images_train_filename_queue, [Images_enqueue] * num_threads))
+
+  [Info_train, Bbox_train] = tf.py_func(read_anno_from_file, [Annotations_train_filename_queue.dequeue()], [tf.float32, tf.float32])
+  
+  image_reader = tf.WholeFileReader()
+  _, image_file = image_reader.read(Images_train_filename_queue)
+  image = tf.image.decode_jpeg(image_file)
+  image = tf.image.convert_image_dtype(image, tf.float32)
+
+  image = tf.Print(image, [ImageSets_train_filename_queue.size(), Annotations_train_filename_queue.size(), Images_train_filename_queue.size()])
+
+  frame_queue = tf.FIFOQueue(1000, dtypes=[Bbox_train.dtype, image.dtype])
+
+  frame_enqueue = frame_queue.enqueue([Bbox_train, image])
+  tf.train.add_queue_runner(
+          tf.train.QueueRunner(frame_queue, [frame_enqueue] * num_threads))
 
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -49,10 +76,10 @@ def main(_):
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
 
-    for i in range(120):
+    for i in range(200):
       # Retrieve a single instance:
-      filename, anno = sess.run([Annotations_train_filename, Annotations_train])
-      print i, filename, anno
+      [frame_anno, frame_image] = sess.run(frame_queue.dequeue())
+      print i, frame_anno, frame_image.shape
 
     coord.request_stop()
     coord.join(threads)
---
+++ sequences.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+def get_seq_frames(seq_t):
+    pass
+
+def get_seq_annos(seq_t):
+    pass
+
+
